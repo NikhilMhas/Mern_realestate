ce6da06bea475fc73b834f8a22ea5461d7df6519
Nikhil Mhaske <105561400+NikhilMhas@users.noreply.github.com>
2024-07-28 21:48:10 +0530

    Initial commit8

--- api/app.js
+++ api/app.js
@@ -25,24 +25,6 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-/////////////Deployment code/////////
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-////////////////////////////////////
-
 app.listen(8800, () => {
   console.log("Server is running!");
 });
